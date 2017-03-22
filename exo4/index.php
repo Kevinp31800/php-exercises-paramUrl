08498d011758fbc38d3d850320c6fb2eb0a92f54
kevin <kevinpuyol@live.fr>
2017-03-22 11:04:07 +0100

    exo4

---
+++ exo4/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>exo4</title>
+</head>
+<body>
+	<?php 
+
+	echo $_GET['langage']." ";
+	echo $_GET['serveur'];
+
+	?>
+</body>
+</html>
